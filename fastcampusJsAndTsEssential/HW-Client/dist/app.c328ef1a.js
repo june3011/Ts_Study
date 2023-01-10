d4725d3bcacc0f1aa1c7c4d50bf40779cb96dbdc
username <81845513+june3011@users.noreply.github.com>
2023-01-10 23:34:34 +0900

    Study : 페이징 구현, 세세한 버그 수정

--- fastcampusJsAndTsEssential/HW-Client/dist/app.c328ef1a.js
+++ fastcampusJsAndTsEssential/HW-Client/dist/app.c328ef1a.js
@@ -123,6 +123,9 @@ var ajax = new XMLHttpRequest();
 var content = document.createElement("div");
 var NEWS_URL = "https://api.hnpwa.com/v0/news/1.json";
 var CONTENT_URL = "https://api.hnpwa.com/v0/item/@id.json";
+var store = {
+  currentPage: 1
+};
 function getData(url) {
   ajax.open("GET", url, false);
   ajax.send();
@@ -132,20 +135,24 @@ function newsFeed() {
   var newPeed = getData(NEWS_URL);
   var newsList = [];
   newsList.push("<ul>");
-  for (var i = 0; i < newPeed.length; i++) {
-    newsList.push("\n      <li>\n        <a href=\"#".concat(newPeed[i].id, "\">\n        ").concat(newPeed[i].title, " {").concat(newPeed[i].comments_count, "}\n        </a>\n      </li>\n      "));
+  for (var i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+    newsList.push("\n      <li>\n        <a href=\"#/show/".concat(newPeed[i].id, "\">\n        ").concat(newPeed[i].title, " {").concat(newPeed[i].comments_count, "}\n        </a>\n      </li>\n      "));
   }
   newsList.push("</ul>");
+  newsList.push("\n    <div>\n        <a href=\"#/page/".concat(store.currentPage > 1 ? store.currentPage - 1 : 1, "\">\uC774\uC804 \uD398\uC774\uC9C0</a>\n        <a href=\"#/page/").concat(newPeed.length / 10 > store.currentPage ? store.currentPage + 1 : store.currentPage, "\">\uB2E4\uC74C \uD398\uC774\uC9C0</a>\n    </div>\n  "));
   container.innerHTML = newsList.join("");
 }
 function newsDetail() {
-  var id = location.hash.substring(1);
+  var id = location.hash.substring(7);
   var newsContent = getData(CONTENT_URL.replace("@id", id));
-  container.innerHTML = "\n      <h1>".concat(newsContent.title, "</h1>\n      <div>\n          <a href=\"#\">\uBAA9\uB85D\uC73C\uB85C</a>\n      </div>\n    ");
+  container.innerHTML = "\n      <h1>".concat(newsContent.title, "</h1>\n      <div>\n          <a href=\"#/page/").concat(store.currentPage, "\">\uBAA9\uB85D\uC73C\uB85C</a>\n      </div>\n    ");
 }
 function router() {
   var routePath = location.hash;
   if (routePath === "") {
+    newsFeed();
+  } else if (routePath.indexOf("#/page/") >= 0) {
+    store.currentPage = Number(routePath.substring(7));
     newsFeed();
   } else {
     newsDetail();
