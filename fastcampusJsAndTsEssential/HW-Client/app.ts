8d924e187d5413d7cb61f9d165ed57a6653ba405
username <81845513+june3011@users.noreply.github.com>
2023-01-20 23:10:18 +0900

    Study : 타입과 인터페이스, 타입 -> 인터페이스의 상속으로 변경

--- fastcampusJsAndTsEssential/HW-Client/app.ts
+++ fastcampusJsAndTsEssential/HW-Client/app.ts
@@ -1,31 +1,31 @@
-type Store = {
+interface Store {
   currentPage: number;
   feeds: [];
-};
+}
 
-type News = {
-  id: number;
-  time_ago: string;
-  title: string;
-  url: string;
-  user: string;
-  content: string;
-};
+interface News {
+  readonly id: number;
+  readonly time_ago: string;
+  readonly title: string;
+  readonly url: string;
+  readonly user: string;
+  readonly content: string;
+}
 
-type NewsFeed = News & {
-  comments_count: number;
-  positions: number;
-  read: boolean;
-};
+interface NewsFeed extends News {
+  readonly comments_count: number;
+  readonly positions: number;
+  read?: boolean;
+}
 
-type NewsDetail = News & {
-  comments: NewsComment[];
-};
+interface NewsDetail extends News {
+  readonly comments: NewsComment[];
+}
 
-type NewsComment = News & {
-  comments: NewsComment[];
-  level: number;
-};
+interface NewsComment extends News {
+  readonly comments: NewsComment[];
+  readonly level: number;
+}
 
 const container: HTMLElement | null = document.getElementById("root");
 const ajax: XMLHttpRequest = new XMLHttpRequest();
@@ -46,7 +46,7 @@ function getData<AjaxResponse>(url: string): AjaxResponse {
 
 function makeFeeds(feeds: NewsFeed[]): NewsFeed[] {
   for (let i = 0; i < feeds.length; i++) {
-    feeds.read = false;
+    feeds[i].read = false;
   }
 
   return feeds;
